106da11af1ff2a8ddb79cd89454e5db94bc8b40f
Julian V. Modesto <julianvmodesto@gmail.com>
2015-07-21 20:12:58 -0400

    Parse NYC Restaurant Week site instead of Open Table site

--- nycrw.js
+++ nycrw.js
@@ -8,45 +8,55 @@ var yelp = require('yelp').createClient({
 });
 var async = require('async');
 
-// scrape the Open Table NYC Restaurant Week website
-request('http://www.opentable.com/promo.aspx?pid=69&m=8', function(error, response, html) {
+// scrape the NYC Restaurant Week website
+request('http://m.nycgo.com/rw', function(error, response, html) {
 	if (!error && response.statusCode == 200) {
 		var $ = cheerio.load(html, {
 			normalizeWhitespace: true,
 			decodeEntities: true
 		});
+
 		var restaurants = [];
 		var calls = [];
 
-		$('div.rest-row-info').each(function(i, element) {
-			var restaurant = $(this).find($('a.rest-name')).text().trim();
-			var meta = $(this).find($('div.rest-row-meta')).text().trim().split('|');
-			calls.push(function(callback) {
-				yelp.search({
-					term: restaurant,
-					location: 'New York, NY',
-					limit: 1
-				}, function(error, data) {
-					if (!error && data.total > 0) {
-						var yelp_business = data.businesses[0];
+		$('li.arrow').each(function(i, element) {
+			var restaurant = $(this).find($('a')).text().trim();
+			var neighborhood = $(this).find($('div.venue_distance')).text().trim();
+			var mealtypes = $(this).find($('div.venue_mealtypes')).text().trim();
 
-						restaurants.push({
-							name: restaurant,
-							yelp_name: yelp_business.name,
-							yelp_rating: yelp_business.rating,
-							yelp_review_count: yelp_business.review_count,
-							cuisine: meta[0].trim(),
-							district: meta[1].trim()
-						});
-					}
-					callback(null);
+			// filter out only M-F and Sunday lunch / brunch
+			// filter out bad Yelp results
+			if ((mealtypes.indexOf('Lunch') > -1 || mealtypes.indexOf('L/B') > -1)) {
+				var search_term = (restaurant.indexOf(' - ') > -1) ? restaurant.substring(0, restaurant.indexOf(' - ')) : restaurant;
+
+				calls.push(function(callback) {
+					yelp.search({
+						term: search_term,
+						location: 'New York, NY',
+						limit: 1
+					}, function(error, data) {
+						if (!error && data.total > 0) {
+							var yelp_business = data.businesses[0];
+							restaurants.push({
+								name: restaurant,
+								neighborhood: neighborhood,
+								mealtypes: mealtypes,
+								yelp_name: yelp_business.name,
+								yelp_rating: yelp_business.rating,
+								yelp_review_count: yelp_business.review_count
+							});
+						}
+						callback(null);
+					});
 				});
-			});
-
+			}
 		});
 
 		async.parallel(calls, function(err) {
 			if (err) return err;
+			restaurants = restaurants.filter(function(el) {
+				return el.yelp_rating >= 4 && el.yelp_review_count > 150;
+			});
 			restaurants.sort(function(a, b) {
 				if (a.yelp_rating == b.yelp_rating) {
 					return (a.yelp_review_count > b.yelp_review_count) ? -1 : (a.yelp_review_count < b.yelp_review_count) ? 1 : 0;
